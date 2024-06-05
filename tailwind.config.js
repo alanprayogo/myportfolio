1cb241708be7870dbddb1bcb22f9ad7948030121
alanprayogo <alanbp98@gmail.com>
2024-06-06 01:10:14 +0700

    hero and about section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,7 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    container: {
-      center: true,
-      padding: "36px",
-    },
-    extend: {
-      screens: {
-        "2xl": "1320px",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
