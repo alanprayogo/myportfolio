68336e57e41d716ad2b9f9db343dc51b91a8627f
alanprayogo <alanbp98@gmail.com>
2024-06-08 22:46:51 +0700

    fix bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,15 @@
 module.exports = {
   content: ["index.html"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       colors: {
-        "dblue": "#1e3a8a",
-        "lblue": "#0ea5e9",
-        "dark": "#0f172a",
+        dblue: "#1e3a8a",
+        lblue: "#0ea5e9",
+        dark: "#0f172a",
       },
     },
   },
